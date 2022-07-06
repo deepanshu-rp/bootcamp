1cb40cecc78043db7db0e53ed89dd513513631a7
deepanshu-rp <deepanshu.yadav@razorpay.com>
2022-07-06 22:21:17 +0530

    Added go solution: Bank Transactions

---
+++ Golang - Day2/exercise3.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+
+func deposit(balance *int, amount int, mu *sync.Mutex, wg *sync.WaitGroup) {
+	mu.Lock()
+	*balance += amount
+	fmt.Println("Previous:", *balance, "Current:", *balance+100)
+	mu.Unlock()
+
+	wg.Done()
+}
+
+
+func withdraw(balance *int, amount int, mu *sync.Mutex, wg *sync.WaitGroup) {
+	mu.Lock()
+	// update balance if transaction valid else throw error
+	if v := *balance-amount; v>= 0 {
+		fmt.Println(*balance, amount, *balance-amount)
+		*balance = v
+		fmt.Println("Previous:", *balance+100, "Current:", *balance)
+
+	} else {
+		panic(any("Invalid transaction"))
+	}
+	mu.Unlock()
+
+	wg.Done()
+}
+
+func main() {
+	var balance = 500
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
+	const iterations = 6
+	wg.Add(2*iterations)
+
+	for i:=0; i<iterations; i++ {
+		go withdraw(&balance,100, &mu, &wg)
+		go deposit(&balance,100, &mu, &wg)
+	}
+
+	wg.Wait()
+
+	fmt.Println(balance)
+
+}
