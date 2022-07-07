96e3f2aa38068e87ba7ea95e434a131d6730e40c
deepanshu-rp <deepanshu.yadav@razorpay.com>
2022-07-07 17:46:23 +0530

    fixed print statements

--- Golang - Day2/exercise3.go
+++ Golang - Day2/exercise3.go
@@ -5,25 +5,23 @@ import (
 	"sync"
 )
 
-
 func deposit(balance *int, amount int, mu *sync.Mutex, wg *sync.WaitGroup) {
 	mu.Lock()
 	fmt.Println(*balance, amount, *balance+amount)
 	*balance += amount
-	fmt.Println("Previous:", *balance-100, "Current:", *balance)
+	fmt.Println("Previous:", *balance-amount, "Current:", *balance)
 	mu.Unlock()
 
 	wg.Done()
 }
 
-
 func withdraw(balance *int, amount int, mu *sync.Mutex, wg *sync.WaitGroup) {
 	mu.Lock()
 	// update balance if transaction valid else throw error
-	if v := *balance-amount; v>= 0 {
+	if v := *balance - amount; v >= 0 {
 		fmt.Println(*balance, amount, *balance-amount)
 		*balance = v
-		fmt.Println("Previous:", *balance+100, "Current:", *balance)
+		fmt.Println("Previous:", *balance+amount, "Current:", *balance)
 
 	} else {
 		panic(any("Invalid transaction"))
@@ -39,11 +37,11 @@ func main() {
 	var mu sync.Mutex
 
 	const iterations = 6
-	wg.Add(2*iterations)
+	wg.Add(2 * iterations)
 
-	for i:=0; i<iterations; i++ {
-		go withdraw(&balance,100, &mu, &wg)
-		go deposit(&balance,100, &mu, &wg)
+	for i := 0; i < iterations; i++ {
+		go withdraw(&balance, 100, &mu, &wg)
+		go deposit(&balance, 100, &mu, &wg)
 	}
 
 	wg.Wait()
