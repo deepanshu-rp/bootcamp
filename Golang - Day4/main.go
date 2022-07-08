185f0ee4d1db4143a5906dd7a6f231570f4fd57e
deepanshu-rp <deepanshu.yadav@razorpay.com>
2022-07-08 13:58:09 +0530

    Setup DB Connection

--- Golang - Day4/main.go
+++ Golang - Day4/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"ecommerce/config"
+
+	"github.com/jinzhu/gorm"
+)
+
 func main() {
+	var err error
+
+	// Connect to database
+	config.DB, err = gorm.Open("mysql", config.DBUrl(config.DBSetup()))
+
+	if err != nil {
+		panic(any("Database error"))
+	}
+
+	defer config.DB.Close()
 
 }
